a2a56420d78ec23642d82f623d4146d73f586035
user <user@LAPTOP-OT1FSN56>
2018-10-26 10:25:19 +0800

    add work1019

--- my1005/my1005/Program.cs
+++ my1005/my1005/Program.cs
@@ -1,60 +1,47 @@
 ﻿using OpenDataImport.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 
 namespace OpenDataImport
 {
-    public class Porgram
+    class Program
     {
         static void Main(string[] args)
         {
-            var nodes = FindOpenData();
-            ShowOpenData(nodes);
+            Service.ImportService importService = new Service.ImportService();
+
+            var nodes = importService.FindOpenData();
+
+            importService.ImportToDb(nodes);
+
+            showOpenData(nodes);
             Console.ReadKey();
 
         }
-        static List<OpenData> FindOpenData()
+
+
+
+
+
+        private static void showOpenData(List<OpenData> nodes)
         {
-            List<OpenData> result = new List<OpenData>();
 
-            var ss = XElement.Load(@"C:\Users\user\Desktop\軟體工程\data.xml");
-            var xml = XElement.Load(@"C:\Users\user\Desktop\軟體工程\data.xml");
-
-            var nodes = xml.Descendants("node").ToList();
-
-            for (var i = 0; i < nodes.Count; i++)
-            {
-                var node = nodes[i];
-                OpenData item = new OpenData();
-
-                item.id = int.Parse(getValue(node, "id"));
-                item.資料集名稱 = getValue(node, "資料集名稱");
-                item.主要欄位說明 = getValue(node, "主要欄位說明");
-                item.服務分類 = getValue(node, "服務分類");
-                result.Add(item);
-            }
-            return result;
-        }
-
-        private static string getValue(XElement node, string propertyName)
-        {
-            return node.Element(propertyName)?.Value?.Trim();
-        }
-
-        public static void ShowOpenData(List<OpenData> nodes)
-        {
-            //Console.WriteLine(string.Format("共收到{0}筆的資料", nodes.Count));
-            nodes.GroupBy(node => node.資料集名稱).ToList()
+            Console.WriteLine(string.Format("共收到{0}筆的資料", nodes.Count));
+            nodes.GroupBy(node => node.服務分類).ToList()
                 .ForEach(group =>
                 {
+
                     var key = group.Key;
                     var groupDatas = group.ToList();
-                    var message = $"資料集名稱:{key},共有{groupDatas.Count()}筆資料";
+                    var message = $"服務分類:{key},共有{groupDatas.Count()}筆資料";
                     Console.WriteLine(message);
-
                 });
+
+
         }
     }
+
 }
